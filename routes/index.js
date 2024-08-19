0b75046cb251a9a5ed779e70ff784fcc5d849f44
Ben Medcalf <ben.medcalf@gmail.com>
2024-08-19 16:46:18 -0400

    update view, show headers, add gitignore, use nodemon

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ben Medcalf gets CeRtIfIeD' });
+router.get('/', function (req, res, next) {
+  console.log("hello from index.js router.get")
+  res.render('index', { title: 'Ben Medcalf gets certified', header: JSON.stringify(req.headers) });
 });
 
 module.exports = router;
